623f19f1e3cb419f05bac31b3538d862598a6531
michael <michael.cjames@hotmail.com>
2024-10-22 13:47:27 +0100

    first commit

---
+++ src/spotifyLogin.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+
+const SpotifyLogin = () => {
+  const [token, setToken] = useState("");
+  const [tracks, setTracks] = useState([]);
+
+  const client_id = "144ebf716b5443ef9600a60b86a47ea0"; // Replace with your Client ID
+  const redirect_uri =
+    "https://007c-2a02-8084-282-ef80-5d79-68c1-3882-a6c2.ngrok-free.app/callback"; // Replace with your Redirect URI
+  const auth_endpoint = "https://accounts.spotify.com/authorize";
+  const response_type = "token";
+  const scope = "user-read-currently-playing"; // You can modify this scope based on your needs
+
+  const getAuthUrl = () => {
+    return `${auth_endpoint}?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=${response_type}&scope=${scope}`;
+  };
+
+  // Function to get the token from the URL after login
+  useEffect(() => {
+    const hash = window.location.hash;
+    let token = window.localStorage.getItem("token");
+
+    if (!token && hash) {
+      token = hash
+        .substring(1)
+        .split("&")
+        .find((elem) => elem.startsWith("access_token"))
+        .split("=")[1];
+      window.location.hash = "";
+      window.localStorage.setItem("token", token);
+    }
+
+    setToken(token);
+
+    // Fetch user's top tracks if token exists
+    if (token) {
+      fetch("https://api.spotify.com/v1/me/player/currently-playing", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((response) => response.json())
+
+        .catch((error) => console.error("Error fetching top tracks:", error));
+    }
+  }, []);
+
+  // Function to logout by removing the token
+  const handleLogout = () => {
+    setToken("");
+    window.localStorage.removeItem("token");
+  };
+
+  return (
+    <div>
+      <h1>Spotify API Example</h1>
+      {!token ? (
+        <a href={getAuthUrl()}>Login to Spotify</a>
+      ) : (
+        <button onClick={handleLogout}>Logout</button>
+      )}
+
+      {token && (
+        <div>
+          <h2>Your Top Tracks</h2>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SpotifyLogin;
